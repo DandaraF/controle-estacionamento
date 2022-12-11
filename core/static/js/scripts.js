aeb032408449dd73f714bd8027f3c2dd1bfaf978
DandaraF <dandara-chr@hotmail.com>
2022-12-11 15:50:49 -0300

    organization code

--- core/static/js/scripts.js
+++ core/static/js/scripts.js
@@ -1,9 +1,9 @@
 $( document ).ready(function() {
     let deleteBtn = $('.delete-btn');
     let outputBtn = $('.output-btn');
+    let editBtn = $('.edit-btn');
     let searchBtn = $('#search-btn');
     let searchForm = $('#search-form');
-    let paymentBtn = $('.payment-btn');
 
     $(deleteBtn).on('click', function(e) {
 
@@ -21,6 +21,18 @@ $( document ).ready(function() {
         searchForm.submit();
     });
 
+    $(editBtn).on('click', function(e) {
+        e.preventDefault();
+        window.location.href = "http://127.0.0.1:8000";
+
+
+        let result = confirm('Deseja alterar os dados do veículo?');
+
+        if(result) {
+            window.location.href = delLink;
+        }
+    });
+
     $(outputBtn).on('click', function(e) {
         e.preventDefault();
 
@@ -31,7 +43,5 @@ $( document ).ready(function() {
             window.location.href = delLink;
         }
     });
-
-
 
 });
--- core/views.py
+++ core/views.py
@@ -51,12 +51,14 @@ class ParkingViewSet(viewsets.ViewSet):
         search = request.GET.get('search')
 
         if search:
-            parkings = Parking.objects.filter(code__icontains=search)
+            parkings = Parking.objects.filter(
+                plate__icontains=search).order_by("-date_input")
 
         else:
-            parkings_list = Parking.objects.filter(left=False)
+            parkings_list = Parking.objects.filter(left=False).order_by(
+                "-paid")
 
-            paginator = Paginator(parkings_list, 10)
+            paginator = Paginator(parkings_list, 12)
 
             page = request.GET.get('page')
 
@@ -162,110 +164,3 @@ class ParkingViewSet(viewsets.ViewSet):
 
         return render(request, 'historic/historic.html',
                       {"parkings": parkings})
-
-        # @staticmethod
-        # def newParking(request):
-        #     if request.method == "POST":
-        #         form = ParkingForm(request.POST)
-        #
-        #         if form.is_valid():
-        #             parking = form.save()
-        #             parking.save()
-        #             return redirect('/')
-        #     else:
-        #         form = ParkingForm()
-        #         return render(request, 'parking/add_input.html', {'form': form})
-        #
-        #
-        # def editParking(request, id):
-        #     parking = get_object_or_404(Parking, pk=id)
-        #     form = ParkingForm(instance=parking)
-        #
-        #     if request.method == 'POST':
-        #         form = ParkingForm(request.POST, instance=parking)
-        #
-        #         if form.is_valid():
-        #             parking.save()
-        #             return redirect('/')
-        #         else:
-        #             return render(request, 'parking/edit_parking.html',
-        #                           {'form': form, 'parking': parking})
-        #
-        #     else:
-        #         return render(request, 'parking/edit_parking.html',
-        #                       {'form': form, 'parking': parking})
-        #
-        #
-        # def deleteParking(request, id):
-        #     parking = get_object_or_404(Parking, pk=id)
-        #     parking.delete()
-        #
-        #     messages.info(request, 'Entrada deletada com sucesso.')
-        #
-        #     return redirect('/')
-        #
-        #
-        # def outParking(request, id):
-        #     parking = get_object_or_404(Parking, pk=id)
-        #
-        #     if parking.paid:
-        #         parking.left = True
-        #         parking.save()
-        #         messages.success(request, 'Saída permitida!')
-        #
-        #     else:
-        #         messages.warning(request, 'Saída não permitida. Pagamento em aberto!')
-        #
-        #     return redirect('/')
-        #
-        #
-        # def payParking(request, id):
-        #     parking = get_object_or_404(Parking, pk=id)
-        #     form = PaymentForm(instance=parking)
-        #
-        #     if request.method == 'POST':
-        #         form = PaymentForm(request.POST, instance=parking)
-        #
-        #         if form.is_valid():
-        #             parking.save()
-        #             return redirect('/')
-        #
-        #     else:
-        #         return render(request, 'parking/payment.html',
-        #                       {'form': form, 'parking': parking})
-        #
-        #
-        # def payment(request, id):
-        #     parking = Parking.objects.get(id=id)
-        #     now = datetime.now()
-        #
-        #     datetime_payment = now - timedelta(hours=parking.date_input.hour,
-        #                                        minutes=parking.date_input.minute,
-        #                                        seconds=parking.date_input.second)
-        #     total_hour = datetime_payment.hour
-        #
-        #     if total_hour <= 1:
-        #         parking.value = 2
-        #
-        #     if 3 >= total_hour > 1:
-        #         parking.value = 4
-        #
-        #     if total_hour > 3:
-        #         parking.value = 4
-        #
-        #     parking.paid = True
-        #     parking.date_output = now
-        #     parking.save()
-        #     return redirect('/')
-        #
-        #
-        # def historicParking(request, plate):
-        #     parkings_list = Parking.objects.filter(plate=plate)
-        #
-        #     paginator = Paginator(parkings_list, 10)
-        #
-        #     page = request.GET.get('page')
-        #
-        #     parkings = paginator.get_page(page)
-        #
-        #     return render(request, 'parking/historic.html', {"parkings": parkings})
